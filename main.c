6de6b6a0a69439aade88cf0befa5bab2d6af0bb6
DatDevNeridian <user.alex3659@gmail.com>
2025-06-20 17:10:02 -0700

    wefwreghytm

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <cstdio>
 #include <Windows.h>
 #include <winnt.h>
+#include <WinUser.h>
+#include <windows.h>
 #include "main.h"
-
+ 
 int main()
-{   
+{
     printHelloBasic();
-    
+
     system("pause");
 
     return 0;
